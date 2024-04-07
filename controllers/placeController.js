cf9eeabf7284488fdbd0a33c805e2bb93f7ac0bf
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-04-06 23:04:03 -0400

    Add Place controllers

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -2,12 +2,97 @@ const asyncHandler = require('express-async-handler');
 const Place = require('../models/place');
 const { body, validationResult } = require('express-validator');
 
-exports.getPlaces = asyncHandler(async (req, res) => {});
+exports.getPlaces = asyncHandler(async (req, res) => {
+  console.log('Hi');
+  const places = await Place.find().sort({ title: 1 }).exec();
+  res.status(200).json({ status: 'success', data: { places } });
+});
 
-exports.getPlace = asyncHandler(async (req, res) => {});
+exports.getPlace = asyncHandler(async (req, res) => {
+  const place = await Place.findById(req.params.id).exec();
 
-exports.createPlace = [asyncHandler(async (req, res) => {})];
+  if (!place) {
+    res.status(404).json({
+      status: 'error',
+      message: 'Place does not exist.',
+    });
+    return;
+  }
 
-exports.updatePlace = [asyncHandler(async (req, res) => {})];
+  res.status(200).json({
+    status: 'success',
+    data: { place },
+  });
+});
 
-exports.deletePlace = asyncHandler(async (req, res) => {});
+exports.createPlace = [
+  body('name').trim().escape(),
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: 'fail',
+        data: errors,
+      });
+      return;
+    }
+
+    const place = new Place({
+      name: req.body.name,
+    });
+
+    const newPlace = await place.save();
+    res.status(200).json({
+      status: 'success',
+      data: { place: newPlace },
+    });
+  }),
+];
+
+exports.updatePlace = [
+  body('name').trim().escape(),
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: 'fail',
+        data: errors,
+      });
+      return;
+    }
+
+    const id = req.params.id;
+    const payload = { name: req.body.name };
+
+    const place = await Place.findByIdAndUpdate(id, payload, { new: true });
+
+    if (!place) {
+      res.status(404).json({
+        status: 'error',
+        message: 'Place does not exist.',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: { place },
+    });
+  }),
+];
+
+exports.deletePlace = asyncHandler(async (req, res) => {
+  const place = await Place.findByIdAndDelete(req.params.id).exec();
+
+  if (!place) {
+    res.status(404).json({
+      status: 'error',
+      message: 'Place does not exist.',
+    });
+    return;
+  }
+
+  res.status(200).json({ status: 'success', data: null });
+});
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const placeRouter = require('./place');
 
-router.use('/place', placeRouter);
+router.use('/places', placeRouter);
 
 module.exports = router;
