457764a6a185307fafd33e56f343ed2f1e4ef2ab
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-04-06 22:51:07 -0300

    Add Place schema

--- models/zone.js
+++ models/zone.js
@@ -4,13 +4,8 @@ const { Schema } = mongoose;
 
 const PlaceSchema = new Schema({
   name: { type: String, required: true },
-  zone: { type: Schema.Types.ObjectId, ref: 'Zone' },
   description: { type: String },
   imageUrl: { type: String },
-  type: { type: String },
-  importance: { type: Number },
-  lat: { type: Number },
-  lng: { type: Number },
 });
 
 module.exports = mongoose.model('Place', PlaceSchema);
